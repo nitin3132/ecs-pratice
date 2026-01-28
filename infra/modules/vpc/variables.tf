c68baf7355485f4c1a5304519a836cc63331337c
Nitin Sharma <tofu@Nitins-MacBook-Air.local>
2026-01-28 14:46:56 -0500

    Fix Terraform variable syntax errors

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -4,7 +4,17 @@ variable "azs" { type = list(string) }
 variable "public_subnets" { type = list(string) }
 variable "private_subnets" { type = list(string) }
 
-variable "enable_nat_gateway" { type = bool, default = true }
-variable "single_nat_gateway" { type = bool, default = true }
+variable "enable_nat_gateway" {
+  type    = bool
+  default = true
+}
 
-variable "tags" { type = map(string), default = {} }
+variable "single_nat_gateway" {
+  type    = bool
+  default = true
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
